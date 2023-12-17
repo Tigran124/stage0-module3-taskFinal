1782dba45c14b4cf353db3691579a76bf85f577b
Tigran124 <tigran.h.988@gmail.com>
2023-12-17 19:06:07 +0400

    changes committed

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,18 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void main(String[] args) {
+        calculateSum(1234);
+    }
+    public static void calculateSum(int number) {
+        int first = number % 10;
+        number /= 10;
+        int second = number % 10;
+        number /= 10;
+        int third = number % 10;
+        number /= 10;
+        int fourth = number % 10;
+        number /= 10;
+        System.out.println(first + second + third + fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String num = String.valueOf(number);
+        System.out.println(num.charAt(2) + "" + num.charAt(1) + num.charAt(0));
     }
 }
